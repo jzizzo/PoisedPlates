627eaef440d481b882b0ec2e38aa6c326d3f5080
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-31 16:39:22 -0700

    (feat) Add simple redux landing page, add redux folder structure to client, add dependencies(axios, react-redux, react-router-dom, redux, redux-promise), refactor webpack to point to src/index.js,

---
+++ client/src/components/browse_index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class BrowseIndex extends Component {
+  render() {
+    return (
+      <div>Preparing to drop plates...</div>
+    )
+  }
+}
---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
+
+import BrowseIndex from './components/browse_index';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Route path="/" component={BrowseIndex} />
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
+
+
