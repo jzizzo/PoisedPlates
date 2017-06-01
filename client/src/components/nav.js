92f26eec7de07230a78cc5cb938ab7cfebeb9590
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-31 23:07:16 -0700

    (feat) add links to browse_products that lead to product page, add home and post links to nav, move Nav into BrowserRouter in index.js to give ability to use Link, and delete browse_list

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default (props) => {
-  return ( <div>Nav goes here</div>)
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        <div>Nav goes here</div>
+        <Link to="/post/new">Post your stuff here</Link>
+      </div>
+    );
+  }
 }
