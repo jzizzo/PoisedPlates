9dabc85b103ed5a559c5c5f2fc0e83a93d1d7d09
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-31 19:52:19 -0700

    (feat) add basic client skeleton

---
+++ client/src/containers/submit_product.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SubmitProduct extends Component {
+  render() {
+    return (
+      <h1>Welcome to the Submit Product Page!</h1>
+    )
+  }
+};
