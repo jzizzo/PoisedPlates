fa07147c9c7f9cea7dd155965d1cf66ac0a0ce27
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-06-02 16:27:05 -0700

    (feat) Connect to API to receive auctions, add action creator, reducer, and connection for BrowseAuctions, and replace mock data with data from database.

--- client/src/containers/BrowseAuctions.js
+++ client/src/containers/BrowseAuctions.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import { fetchAuctions } from '../actions';
 import { Link } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { GridList, GridTile } from 'material-ui/GridList';
@@ -35,45 +36,66 @@ class BrowseAuctions extends Component {
     };
   }
 
-  render() {
+  componentDidMount() {
+    this.props.fetchAuctions();
+  }
+
+  renderAuctions() {
     const styles = {
       image: {
         height: '300px',
         objectFit: 'cover'
-      },
-      grid: {
-        margin: '8px',
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
       },
       grow: {
         padding: '4px',
         width: '30rem'
       }
     }
+    return _.map(this.props.auctions, auction => {
+      console.log(auction);
+      return (
+        <div key={auction.id} style={styles.grow}>
+          <Link to={`/auction/${auction.id}`}>
+            <MuiThemeProvider>
+              <GridTile
+                key={auction.id}
+                title={auction.title}
+              >
+                <img src={auction.images[0].url} style={styles.image}/>
+              </GridTile>
+            </MuiThemeProvider>
+          </Link>
+        </div>
+      )
+    });
+  }
+
+  render() {
+    const styles = {
+      grid: {
+          margin: '8px',
+          display: 'flex',
+          flex: 1,
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center'
+        }
+      }
+
     return (
       <div style={styles.grid}>
-        {_.map(this.data, listing => (
-          <div key={listing.id} style={styles.grow}>
-            <Link to={`/auction/${listing.id}`}>
-              <MuiThemeProvider>
-                <GridTile
-                  key={listing.id}
-                  title="<listing.title>"
-                  subtitle="listing.subtitle"
-                >
-                  <img src={listing['img-url']} style={styles.image}/>
-                </GridTile>
-              </MuiThemeProvider>
-            </Link>
-          </div>
-        ))}
+        {this.renderAuctions()}
       </div>
     )
   }
 }
 
-export default BrowseAuctions;
+function mapStateToProps(state) {
+  return { auctions: state.auctions };
+}
+
+export default connect(mapStateToProps, { fetchAuctions })(BrowseAuctions);
+
+
+
+
