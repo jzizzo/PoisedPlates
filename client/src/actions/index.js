1ab8881afce844eb515e4d3f0018605792241d94
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-06-02 19:41:56 -0700

    (feat) Add action creator for posting an auction.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const FETCH_AUCTIONS = 'fetch_auctions';
+export const POST_AUCTION = 'post_auction';
 
 const ROOT_URL = 'http://localhost:3000/api';
 
@@ -11,4 +12,13 @@ export function fetchAuctions() {
     type: FETCH_AUCTIONS,
     payload: request
   }
+}
+
+export function postAuction(values) {
+  const request = axios.get(`${ROOT_URL}/auction`, values);
+
+  return {
+    type: POST_AUCTION,
+    payload: request
+  };
 }
