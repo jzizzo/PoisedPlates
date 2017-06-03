60aca34e6847b1fcfb78514be9f785c5d8d4096a
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-06-02 21:38:11 -0700

    (cleanup) Remove console.log in reducer

--- client/src/reducers/reducer_auctions.js
+++ client/src/reducers/reducer_auctions.js
@@ -4,7 +4,6 @@ import { FETCH_AUCTIONS, FETCH_AUCTION } from '../actions';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_AUCTION:
-    console.log('yo', action)
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_AUCTIONS:
       return _.mapKeys(action.payload.data, 'id');
