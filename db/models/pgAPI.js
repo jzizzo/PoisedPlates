6c3ae72b77f38610206a1f9ed825d832360bcac8
Steve Kim <emailstevenkim@gmail.com>
2017-06-03 11:43:45 -0700

    Refactor server/routers/api; Add pgAPI to db/models/

--- db/models/pgAPI.js
+++ db/models/pgAPI.js
@@ -14,18 +14,109 @@ const pg = {
   },
 
   getAuctionById: (auctionId) => {
-  return models.Auction
-    .where({ id: auctionId })
-    .fetch({
-      withRelated: ['images', 'location', 'auctionOwner', 'bids', 'category', 'bidsProfiles']
-    })
-    .then(auction => {
-      return auction;
-    })
-    .catch(error => {
-      return error;
-    });
-}
+    return models.Auction
+      .where({ id: auctionId })
+      .fetch({
+        withRelated: ['images', 'location', 'auctionOwner', 'bids', 'category', 'bidsProfiles']
+      })
+      .then(auction => {
+        return auction;
+      })
+      .catch(error => {
+        return error;
+      });
+  },
+
+  deleteAuctionById: (auctionId) => {
+    return models.Auction
+      .where({ id: auctionId })
+      .destroy()
+      .then(deletedAuction => {
+        return deletedAuction;
+      })
+      .catch(error => {
+        return error;
+      });
+  },
+
+  getAllCategories: () => {
+    return models.Category
+      .collection()
+      .fetch({
+        columns: ['id', 'name']
+      })
+      .then(categories => {
+        return categories;
+      })
+      .catch(error => {
+        return error;
+      });
+  },
+
+  createAuction: (options) => {
+    // console.log('options for createAuction: ', options);
+    return models.Location
+      .where({ city: options.city, state: options.state })
+      .fetch({
+        columns: ['id']
+      })
+      .then(location => {
+        if (location) {
+          return location;
+        } else { // insert city and state into Locations table
+          return models.Location
+            .forge({
+              city: options.city,
+              state: options.state
+            })
+            .save()
+            .then(newLocation => {
+              // returns new increment of locations table insert
+              return newLocation;
+            })
+            .catch(error => {
+              return error;
+            });
+        }
+      })
+      .then(location => {
+        return models.Category
+          .where({ name: options.category })
+          .fetch({
+            columns: ['id']
+          })
+          .then(category => {
+            return models.Auction
+              .forge({
+                profile_id: 4,
+                // profile_id: options.user,
+                category_id: category.id,
+                location_id: location.id,
+                end_time: options.end_time || new Date(),
+                title: options.title,
+                description: options.description
+              })
+              .save()
+              .then(newAuction => {
+                return models.Image
+                  .forge({
+                    auction_id: newAuction.id,
+                    url: options.url
+                  })
+                  .save()
+                  .then(() => {
+                    return newAuction;
+                  });
+              })
+              .catch(error => {
+                return error;
+              });
+          });
+      })
+      .catch(error => {
+        return error;
+      });
+  }
 };
 
 module.exports = pg;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const models = require('../../db/models');
 const pg = require('../../db/models/pgAPI');
 const middleware = require('../middleware');
 
@@ -25,55 +24,50 @@ router.route('/auctions')
       });
   });
 
-// router.route('/auction/:id')
-//   .get((req, res) => {
-//     pg.getAuctionById(req.params.id)
-//       .then(auctionData => {
-//         res.status(200).send(auctionData);
-//       })
-//       .catch(error => {
-//         res.status(404);
-//       });
-//   })
-//   .delete((req, res) => {
-//     pg.deleteAuctionById(req.params.id)
-//       .then(deletedAuctionData => {
-//         res.status(202).send(deletedAuctionData);
-//       })
-//       .catch(error => {
-//         res.status(410);
-//       });
-//   });
-
 router.route('/auction/:id')
   .get((req, res) => {
-    console.log('req.params.id', req.params.id)
-    return models.Auction.where({ id: req.params.id}).fetch({withRelated: ['images', 'location', 'auctionOwner', 'bids', 'category', 'bidsProfiles']
-    })
+    pg.getAuctionById(req.params.id)
       .then(auctionData => {
-        res.send(auctionData);
+        res.status(200).send(auctionData);
+      })
+      .catch(error => {
+        res.status(404);
       });
   })
-  .delete((req, res) => {
-    return models.Auction
-      .where({ id: req.params.id })
-      .destroy()
-      .then( () => {
-        res.status(202);
+  .delete(middleware.auth.verify, (req, res) => {
+    pg.deleteAuctionById(req.params.id)
+      .then(deletedAuctionData => {
+        res.status(202).send(deletedAuctionData);
+      })
+      .catch(error => {
+        res.status(410);
       });
   });
 
-
-  router.route('/categories')
-    .get((req,res) => {
-      return models.Category.collection()
-      .fetch({
-        columns: ['id', 'name']
+router.route('/categories')
+  .get((req,res) => {
+    pg.getAllCategories()
+      .then(categories => {
+        res.status(200).send(categories);
       })
-      .then(collection => {
-        res.send(collection);
+      .catch(error => {
+        res.status(404);
       });
+  });
+
+router.route('/auction')
+  .post(middleware.auth.verify, (req, res) => {
+    const options = Object.assign({}, req.body, req.session.passport);
+    pg.createAuction(options)
+    .then(() => {
+      res.status(200).redirect('/');
+    })
+    .catch(error => {
+      res.status(401);
     });
+  });
+
+module.exports = router;
 
 // profilebids not currently working
 // after mvp for display whether an auction has a bid
@@ -87,74 +81,3 @@ router.route('/auction/:id')
   //         res.send(collection);
   //       });
   //   });
-
-  const SAMPLE = {
-    img: [],
-    title: 'Original 70s bell bottoms',
-    description: '',
-    city: 'Austin',
-    state: 'Texas',
-    date: new Date(),
-    category: 'camping'
-  };
-
-  router.route('/auction/')
-  //middleware.auth.verify,
-  // put into post before (req, res)
-    .post( (req, res) => {
-        return models.Location.where({city: req.body.city, state: req.body.state})
-        .fetch({
-          columns: ['id']
-        })
-        .then(id => {
-          if (id) {
-            return id;
-          } else { //insert city and state into db
-            return models.Location
-              .forge({
-                city: req.body.city,
-                state: req.body.state
-              })
-              .save()
-              .then(data => {
-                // returns new increment of locations table insert
-                return data.id;
-              });
-          }
-        })
-        .then( locationId => {
-          return models.Category.where({name: req.body.category})
-            .fetch({
-              columns: ['id']
-            })
-            .then( categoryId => {
-              return models.Auction
-                .forge({
-                  // profile_id is dummy as middleware.auth is commented out
-                  profile_id: 4,
-                  category_id: categoryId.id,
-                  location_id: locationId.id,
-                  end_time: req.body.end_time || new Date() ,
-                  title: req.body.title,
-                  description: req.body.description
-                })
-                .save()
-                .then( newAuction => {
-                  return models.Image
-                    .forge({
-                      auction_id: newAuction.id,
-                      url: req.body.url
-                    })
-                    .save()
-                    .then( insertImage => {
-                      res.redirect(202, '/');
-                    })
-                })
-            })
-        }).catch( reason => {
-          console.log(`Handle rejected promise (${reason}) here.`);
-        })
-    });
-
-
-module.exports = router;
