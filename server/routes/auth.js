c670f178e19fb460dfdb56e417275bd036a88bf6
Steve Kim <emailstevenkim@gmail.com>
2017-06-05 16:06:02 -0700

    Update auth to see currentUser on front end

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,12 +5,16 @@ const router = express.Router();
 
 router.route('/')
   .get(middleware.auth.verify, (req, res) => {
-    res.render('index.ejs');
+    // res.render('index.ejs');
+    res.render('index.ejs', { user: req.user ? req.user : null });
   });
 
 router.route('/login')
   .get((req, res) => {
-    res.render('login.ejs', { message: req.flash('loginMessage') });
+    res.render('login.ejs', {
+      message: req.flash('loginMessage'),
+      user: req.user ? req.user : null
+    });
   })
   .post(middleware.passport.authenticate('local-login', {
     successRedirect: '/',
@@ -20,7 +24,10 @@ router.route('/login')
 
 router.route('/signup')
   .get((req, res) => {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
+    res.render('signup.ejs', {
+      message: req.flash('signupMessage'),
+      user: req.user ? req.user : null
+    });
   })
   .post(middleware.passport.authenticate('local-signup', {
     successRedirect: '/profile',
@@ -31,7 +38,8 @@ router.route('/signup')
 router.route('/profile')
   .get(middleware.auth.verify, (req, res) => {
     res.render('profile.ejs', {
-      user: req.user // get the user out of session and pass to template
+      // get the user out of session and pass to template
+      user: req.user ? req.user : null
     });
   });
 
