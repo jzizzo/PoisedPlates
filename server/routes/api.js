7c3b3fa591badefb4a6ff350998a38d29f0ffb14
Steve Kim <emailstevenkim@gmail.com>
2017-06-07 23:45:55 -0700

    Refactor server/database code; Add routes for posting bids and retrieving the current user's bid

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,109 +4,104 @@ const router = express.Router();
 const pg = require('../../db/models/pgAPI');
 const middleware = require('../middleware');
 
-router.route('/')
-  .get((req, res) => {
-    res.status(200).send('Hello World!');
-  })
-  .post((req, res) => {
-    console.log('in the correct route');
-    res.status(201).send({ data: 'Posted!' });
-  });
-
 router.route('/auctions')
   .get((req, res) => {
-    pg.getAllAuctions()
-      .then(auctions => {
-        if (auctions) {
-          console.log(auctions);
-          res.status(200).send(auctions);
-        } else {
-          console.log(auctions);
-          res.status(404);
-        }
-      })
-      .catch(error => {
-        console.log(error);
+    pg.getAllAuctions((err, auctions) => {
+      if (err) {
+        console.log("Couldn't get all auctions: ", err);
         res.status(404);
-      });
+      }
+      res.status(200).send(auctions);
+    });
   });
 
 router.route('/auction/:id')
   .get((req, res) => {
-    pg.getAuctionById(req.params.id)
-      .then(auctionData => {
-        res.status(200).send(auctionData);
-      })
-      .catch(error => {
+    pg.getAuctionById(req.params.id, (err, auction) => {
+      if (err) {
+        console.log("Couldn't get auction info: ", err);
         res.status(404);
-      });
+      }
+      res.status(200).send(auction);
+    });
   })
   .delete(middleware.auth.verify, (req, res) => {
-    pg.deleteAuctionById(req.params.id)
-      .then(deletedAuctionData => {
-        res.status(202).send(deletedAuctionData);
-      })
-      .catch(error => {
+    pg.deleteAuctionById(req.params.id, (err, deletedAuction) => {
+      if (err) {
+        console.log("Couldn't delete auction: ", err);
         res.status(410);
-      });
+      }
+      res.status(202).send(deletedAuction);
+    });
+  })
+  .post((req, res) => {
+    let options = {
+      auctionId: req.params.id,
+      profileId: req.session.passport.user,
+      bidAmt: req.body.amt
+    };
+    pg.postBid(options, (err, bid) => {
+      if (err) {
+        console.log("Couldn't post a bid: ", err);
+        res.status(404);
+      }
+      res.status(200).send(bid);
+    });
   });
 
 router.route('/categories')
   .get((req,res) => {
-    pg.getAllCategories()
-      .then(categories => {
-        res.status(200).send(categories);
-      })
-      .catch(error => {
+    pg.getAllCategories((err, categories) => {
+      if (err) {
         res.status(404);
-      });
+      }
+      res.status(200).send(categories);
+    });
   });
 
 router.route('/auction')
   .post(middleware.auth.verify, (req, res) => {
     const options = Object.assign({}, req.body, req.session.passport);
     // const options = Object.assign({}, req.body); // for test purpose
-    pg.createAuction(options)
-    .then(() => {
+    pg.createAuction(options, (err, auction) => {
+      if (err) {
+        console.log("Couldn't create an auction: ", err);
+        res.status(401);
+      }
       res.status(200).redirect('/');
-    })
-    .catch(error => {
-      res.status(401);
     });
-  });
-
-router.route('/endingAuctions')
-  .get((req, res) => {
-    pg.retrieveAndUpdateEndingAuctions()
-      .then(auctions => {
-        res.send(auctions);
-      });
   });
 
 router.route(middleware.auth.verify, '/auction/:auction_id/currentBid')
   .get((req, res) => {
-    pg.currentUserBid(req.params.auction_id, req.session.passport.user)
-      .then((bidAmt) => {
-        if (bidAmt) {
-          res.status(200).send(bidAmt);
-        }
+    let options = {
+      auctionId: req.params.auction_id,
+      profileId: req.session.passport.user
+    };
+    pg.currentUserBid(options, (err, bid) => {
+      if (err) {
+        console.log("Couldn't get the current bid: ", err);
         res.status(404);
-      });
+      }
+      res.status(200).send(bid);
+    });
   });
 
-
+// testing purposes
+router.route('/endingAuctions')
+  .get((req, res) => {
+    let currentTime = new Date(Date.now());
+    // pg.retrieveAndUpdateEndingAuctions()
+    //   .then(endingAuctions => {
+    //     res.send(endingAuctions);
+    //   });
+    pg.retrieveAndUpdateEndingAuctions(currentTime, (err, endingAuctions) => {
+      if (err) {
+        console.log("Couldn't find ending auctions: ", err);
+        res.status(400);
+      }
+      res.status(200).send(endingAuctions);
+    });
+  });
 
 module.exports = router;
-
-// profilebids not currently working
-// after mvp for display whether an auction has a bid
-  // router.route('/profileBids')
-  //   .get((req, res) => {
-  //     console.log('>>',req)
-  //     return models.ProfileBids.collection().fetch({
-  //       withRelated: ['auctions', 'profiles']
-  //     })
-  //       .then(collection => {
-  //         res.send(collection);
-  //       });
-  //   });
