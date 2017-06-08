a17958fa6bc3a30ebd7b53734ad33b232bfe5d49
Jim Lee <jimleeisme@gmail.com>
2017-06-07 18:46:41 -0700

    (cleanup) Remove unnecessary code

--- client/src/containers/AuctionForm.js
+++ client/src/containers/AuctionForm.js
@@ -59,12 +59,6 @@ const categories = [
 
 // validation functions
 const required = value => (value == null ? 'Required' : undefined);
-const email = value =>
-  (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? 'Invalid email'
-    : undefined);
-
-
 
 
 class AuctionForm extends Component {
