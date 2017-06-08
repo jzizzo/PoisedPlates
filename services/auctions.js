3814bab86915c39ad5dbe24f118165382a88e03b
Steve Kim <emailstevenkim@gmail.com>
2017-06-08 15:41:27 -0700

    Scaffold for cron job

--- services/auctions.js
+++ services/auctions.js
@@ -1,30 +1,14 @@
-const pg = require('./db/models/pgAPI');
+const config = require('config');
+const CronJob = require('cron').CronJob;
+const pg = require('../../db/models/pgAPI');
 
-const fetchEndingAuctions = () => {
-  pg.retrieveEndingAuctions()
-    .then(auctions => {
-      // for each auction id
-      auctions.forEach((auction) => {
-        // get the highest bidder
-        return pg.findHighestBidderForAuction(auction.id)
-          .then(highestBidder => {
-            if (highestBidder) {
-              // email the auction owner and the highest bidder
-              // auction owner: auction.auctionOwner.email
-              // auction first name: auction.auctionOwner.first
-            } else {
-              // email the auction owner that the auction ended
-            }
-          })
-          .then(() => {
-            // update the auction and set ended to false
-          })
-      })
-    })
-    .then(auctions => {
-      // for each auction id
-        // update the ended column to true
-    })
-};
 
-fetchEndingAuctions();
+const processEndingAuctions = new CronJob({
+  cronTime: '00 0-59 * * * *',
+  onTick: function() {
+    console.log("hello world");
+  },
+  start: true
+});
+
+processEndingAuctions.start();
