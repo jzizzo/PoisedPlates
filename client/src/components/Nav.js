8e22e9e8fd2ae169e062bc1ffd7857273ac03d59
Jim Lee <jimleeisme@gmail.com>
2017-06-12 16:03:31 -0700

    (feat) Add floating action button

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -6,8 +6,11 @@ import { Link } from 'react-router-dom';
 /* * Styles * */
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 import FlatButton from 'material-ui/FlatButton';
-import { deepPurple500 } from 'material-ui/styles/colors';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import { deepPurple500, tealA700 } from 'material-ui/styles/colors';
+
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
@@ -24,25 +27,46 @@ const styles = {
   buttons: {
     marginTop: 5,
     color: '#FFF',
+  },
+  fab: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    zIndex: 100,
+    position: 'fixed',
   }
 };
 
 export default class Nav extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <AppBar
-          title={<Link to="/" style={styles.title}>Toss.it</Link>}
-          iconElementRight={
-            <div style={styles.div}>
-              <Link to="/auction">
-                <FlatButton label="Post" style={styles.buttons}/>
-              </Link>
-            </div>
-          }
-          style={styles.nav}
-        />
-      </MuiThemeProvider>
+      <div>
+        <MuiThemeProvider>
+          <AppBar
+            title={<Link to="/" style={styles.title}>Toss.it</Link>}
+            iconElementRight={
+              <div style={styles.div}>
+                <Link to="/auction">
+                  <FlatButton label="Post" style={styles.buttons}/>
+                </Link>
+              </div>
+            }
+            style={styles.nav}
+          />
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <Link to='/auction'>
+            <FloatingActionButton
+              style={styles.fab}
+              backgroundColor={tealA700}
+            >
+              <ContentAdd />
+            </FloatingActionButton>
+          </Link>
+        </MuiThemeProvider>
+      </div>
     );
   }
 }
