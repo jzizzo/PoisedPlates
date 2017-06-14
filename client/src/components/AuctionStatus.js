ccb57c7809c208a86d5037b5b46f17b1174bc30d
douge <dougecox@gmail.com>
2017-06-14 11:26:34 -0700

    count down implemented

--- client/src/components/AuctionStatus.js
+++ client/src/components/AuctionStatus.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import Countdown from 'react-count-down'
 
 /* * Styles * */
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -28,38 +29,6 @@ const styles = {
   },
 };
 
-const tableData = [
-  {
-    name: 'John Smith',
-    status: 'Employed',
-  },
-  {
-    name: 'Randal White',
-    status: 'Unemployed',
-  },
-  {
-    name: 'Stephanie Sanders',
-    status: 'Employed',
-  },
-  {
-    name: 'Steve Brown',
-    status: 'Employed',
-  },
-  {
-    name: 'Joyce Whitten',
-    status: 'Employed',
-  },
-  {
-    name: 'Samuel Roberts',
-    status: 'Employed',
-  },
-  {
-    name: 'Adam Moore',
-    status: 'Employed',
-  },
-];
-const sampleData = [{"id":4,"location_id":4,"end_time":"2017-06-13T20:40:36.119Z","title":"Canon camera"},{"id":5,"location_id":4,"end_time":"2020-08-14T06:27:16.119Z","title":"Apple MacBook Pro 13-inch"},{"id":6,"location_id":4,"end_time":"2020-08-14T06:27:16.119Z","title":"klean kanteen insulated"}];
-
 
 class AuctionStatus extends Component {
   componentDidMount() {
@@ -68,9 +37,15 @@ class AuctionStatus extends Component {
 
   time(end_time) {
     var date = new Date(end_time);
-      console.log( 'a', typeof end_time )
+    const cb = () => console.log('')
+    const OPTIONS = { endDate: date.toString(), prefix: 'for auction', cb}
+ 
+   const TestComponent = () => (
+    <Countdown options={OPTIONS} />
+   )
+      
         // `${date.getHour()}:${date.getMinutes()}:${date.getSeconds()}` 
-    return date
+    return <Countdown options={OPTIONS}/>
   }
   
   render() {
@@ -83,7 +58,6 @@ class AuctionStatus extends Component {
           />
           
           <CardText>
-          {console.log('thisprop',this.props.data)}
           </CardText>
           // make a map function that input dat
             // populate each el in html list for display
@@ -108,7 +82,7 @@ class AuctionStatus extends Component {
             {row.title ? row.title : row.auction.title}
           </TableRowColumn>
           <TableRowColumn >
-           {row.end_time ? (row.end_time) : row.auction.end_time}
+           {row.end_time ? this.time(row.end_time) : this.time(row.auction.end_time)}
           </TableRowColumn>
           <TableRowColumn >
            {row.bid ? row.bid : ""}
