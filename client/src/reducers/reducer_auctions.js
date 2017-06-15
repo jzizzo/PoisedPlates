affb279ad03ca5671a91bc0d787751a29f547dd4
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-06-14 19:54:14 -0700

    (feat) Establish working search functionality

--- client/src/reducers/reducer_auctions.js
+++ client/src/reducers/reducer_auctions.js
@@ -7,14 +7,16 @@ export default function(state = {}, action) {
       let queryResults = {};
       action.payload.data.hits.hits.map( auction => {
         const {_source} = auction;
+        console.log('what up?', _source)
         queryResults[_source.auction_id] = {
           id: _source.auction_id,
           category: { id: _source.category_id, name: _source.name },
           categpry_id: _source.category_id,
           description: _source.description,
           end_time: _source.end_time,
-          images: [ { "auction_id": _source.auction_id, url: _source.url} ],
-          title: _source.title
+          images: [ { auction_id: _source.auction_id, url: _source.url} ],
+          title: _source.title,
+          location: { city: _source.city, id: _source.location_id, state: _source.state}
         }
       })
       return queryResults;
